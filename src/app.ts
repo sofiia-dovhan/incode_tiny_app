7b0531b5cc66b9c206facab276a568c06740891a
Sofiia Dovhan <dovgan014@gmail.com>
2023-02-03 22:43:29 +0200

    mock user crud operations

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import userController from './user.controller';
 
 const app = express();
 
-
+app.use('/users', userController);
 
 
 
---
+++ src/user.controller.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const userController = express.Router();
+
+userController.post('/', (req, res) => {
+  res.json({ toDo: 'should create user' });
+});
+
+userController.put('/:userId', (req, res) => {
+  const { userId } = req.params; 
+  res.json({ toDo: `should update user with ${userId} id` });
+});
+
+userController.delete('/:userId', (req, res) => {
+  const { userId } = req.params; 
+  res.status(204).end();
+});
+
+
+export default userController;
