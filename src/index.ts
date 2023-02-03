f6f7ce7e3a5c610135cfba3d8d0f01819106f8f4
Sofiia Dovhan <dovgan014@gmail.com>
2023-02-03 22:05:48 +0200

    app listen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import app from "./app";
+
+async function main() {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`server is listened on http://localhost:${port}`);
+  });
+};
+
+main();
