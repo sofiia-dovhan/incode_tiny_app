8b9608df22f01b5629bb7936643456dc20e45256
Sofiia Dovhan <dovgan014@gmail.com>
2023-02-09 02:05:15 +0200

    login

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import authController from './auth.controller';
 import userController from './user.controller';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 
@@ -9,7 +12,5 @@ app.use(express.json());
 app.use('/auth', authController);
 
 app.use('/users', userController);
-
-
 
 export default app;
--- src/auth.controller.ts
+++ src/auth.controller.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { Payload } from './payload.type';
 import { Role } from './role.enum';
 import { UserModel } from './user.model';
+import jwt from 'jsonwebtoken';
 
 const authController = express.Router();
 
@@ -11,8 +13,20 @@ authController.post('/registration', async (req, res) => {
   res.json(user);
 });
 
-authController.post('/login', (req, res) => {
-  res.json({ toDo: 'should login user' });
+// TODO add expiration times for tokens
+authController.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+
+  if (!user || password !== user.password) {
+    throw new Error('Email or password is incorrect');
+  }
+
+  const payload: Payload = { _id: user._id };
+  const access = jwt.sign(payload, process.env.JWT_ACCESS_SECRET || 'secret');
+  const refresh = jwt.sign(payload, process.env.JWT_REFRESH_SECRET || 'secret');
+
+  res.json({ access, refresh });
 });
 
 authController.post('/refresh', (req, res) => {
